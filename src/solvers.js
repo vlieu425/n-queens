35bd9cf10278bfa1dafbd3358c13630787c3f2dc
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-02-01 13:27:06 -0800

    implement findNrooksSolution and pass tests. countNRooks in progress.

--- src/solvers.js
+++ src/solvers.js
@@ -16,20 +16,73 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = new Board({n:n}); //fixme
+  var solution = new Board({n: n}); //fixme
+
+  //Recursive function (rowIndex, columnIndex)
+  var placeRooks = function (rowIndex) {
+    //base case
+    //end recrusion when we solved --> when we row = n
+    if (rowIndex === n) {
+      return solution.rows();
+    }
+
+    //Iterate through n, iterate through column
+    for (var col = 0; col < n; col++) {
+      //Place a rook at parameters
+      solution.togglePiece(rowIndex, col);
+
+      //check to see if there's a conflict, if no conflict
+      if (!solution.hasAnyRooksConflicts()) {
+        //new rowIndex = row + 1
+        //new colIndex = i
+        //recursive case on next square(when we have no conflict with placed piece)
+        var r = placeRooks(rowIndex + 1);
+        return r;
+      } else {
+        //Backtrack - retoggle to nothing
+        solution.togglePiece(rowIndex, col);
+      }
+    }
+  };
+
+  //invoke the recursive function at (0)
+  var result = placeRooks(0);
 
 
-
-
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(result));
+  return result;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+window.countNRooksSolutions = function(n, rowIndex) {
+  var solutionCount = 0; //fixme
+  var solution = new Board({n: n});
+  //Recursive function (rowIndex, columnIndex)
+  var findSolutions = function (rowIndex) {
+    //base case
+    //end recrusion when we solved --> when we row = n
+    if (rowIndex === n) {
+      solutionCount++;
+    }
 
+    //Iterate through n, iterate through column
+    for (var col = 0; col < n; col++) {
+      //Place a rook at parameters
+      solution.togglePiece(rowIndex, col);
+
+      //check to see if there's a conflict, if no conflict
+      if (!solution.hasAnyRooksConflicts()) {
+        //new rowIndex = row + 1
+        //new colIndex = i
+        //recursive case on next square(when we have no conflict with placed piece)
+        findSolutions(rowIndex + 1);
+      } else {
+        //Backtrack - retoggle to nothing
+        solution.togglePiece(rowIndex, col);
+      }
+    }
+  };
+  findSolutions(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
